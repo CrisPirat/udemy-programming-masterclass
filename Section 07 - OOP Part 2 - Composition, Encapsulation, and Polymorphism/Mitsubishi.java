dcee5a0bb03b7944fecc4fee86bcad239211767b
unknown <=>
2021-06-12 07:15:56 -0500

    Exercises

---
+++ Section 07 - OOP Part 2 - Composition, Encapsulation, and Polymorphism/Mitsubishi.java
@@ -0,0 +1,17 @@
+public class Mitsubishi extends Car {
+ public Mitsubishi(int cylinders, String name){
+    super(cylinders, name); 
+  }
+  @Override
+  public String startEngine(){
+    return "Mitsubishi -> startEngiin()";
+  }
+  @Override 
+  public String accelerate(){
+    return "Mitsubishi -> accelerate()";
+  }
+  @Override 
+  public String brake(){
+    return "Mitsubishi -> brake()";
+  }
+}
